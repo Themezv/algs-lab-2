2504177f7ff11f6ee06b02ebb940835e530e55f2
Artem Zverev <themezv@gmail.com>
2017-11-01 22:02:21 +0700

    for git

--- src/components/SvgMain.vue
+++ src/components/SvgMain.vue
@@ -1,5 +1,5 @@
 <template>
-    <svg>
+    <svg width="100%" height="100%">
       <line x1="0" y1="0" x2="200" y2="200" stroke-width="2" :stroke="fill"></line>
       <svgLine :x1="10" :y1="20" :x2="210" :y2="230" :stroke-width="2" :stroke="fill"></svgLine>
     </svg>
@@ -18,7 +18,5 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
+
 </style>
